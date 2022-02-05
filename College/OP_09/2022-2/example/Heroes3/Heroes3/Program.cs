f25d449622cdf189be7753c86f9226c00d8bdfad
SunJohnson <sunjonson@yandex.ru>
2022-02-05 10:57:31 +0300

    add example 1

---
+++ College/OP_09/2022-2/example/Heroes3/Heroes3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heroes3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Spearman spearman1 = new Spearman();
+            Spearman spearman2 = new Spearman();
+
+            int attackSpearman1 = spearman1.Storming();
+            int attackSpearman2 = spearman2.Storming();
+
+            while(!spearman1.IsDead() || !spearman2.IsDead())
+            {
+                spearman2.LossOfHealth(attackSpearman1);
+                spearman1.LossOfHealth(attackSpearman2);
+            }
+            Console.WriteLine("Копейщик 1 мертв: {0}",spearman1.IsDead());
+            Console.WriteLine("Копейщик 2 мертв: {0}",spearman2.IsDead());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ College/OP_09/2022-2/example/Heroes3/Heroes3/Spearman.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heroes3
+{
+    internal class Spearman
+    {
+        private int attack = 1;
+        private int defends = 5;
+        private int damage = 1 ;
+        private int health = 10;
+        private int speed = 4;
+        /// <summary>
+        /// Атаковать
+        /// </summary>
+        /// <returns>Сила атаки</returns>
+        public int Storming()
+        {
+            return attack;
+        }
+        public void LossOfHealth(int damage)
+        {
+            health = health - damage;
+        }
+        public bool IsDead()
+        {
+            bool isDead;
+            if(health > 0)
+            {
+                isDead = false;
+            }
+            else
+            {
+                isDead = true;
+            }
+            return isDead;
+        }
+    }
+}
